d589176e0f9957fb3d58e5adb7bfa594758bcf4c
FowlBird13 <bradleympeterson05@gmail.com>
2026-05-06 11:04:56 -0600

    sprites all aligned to the origin

--- grappler.lua
+++ grappler.lua
@@ -3,25 +3,25 @@
 
 -- Create a table with the basic animations
 local sprites = {
-    idle			    = Sprite.new("sGrapplerIdle",       "Sprites/idle.png",     1, 15, 12),
-    walk		        = Sprite.new("sGrapplerWalk",		"Sprites/walk.png",     1, 22, 16),
-    jump		        = Sprite.new("sGrapplerJump",		"Sprites/jump.png",     1, 20, 10),
-    jump_peak	        = Sprite.new("sGrapplerJumpPeak",	"Sprites/jumppeak.png", 1, 20, 10),
-    lfall		        = Sprite.new("sGrapplerFall",		"Sprites/fall.png",     1, 20, 10),
-    climb		        = Sprite.new("sGrapplerClimb",		"Sprites/climb.png",    1, 21, 28),
-    climb_hurt	        = Sprite.new("sGrapplerClimbHurt",	"Sprites/climb.png",    1, 21, 28),
-    death		        = Sprite.new("sGrapplerDeath",		"Sprites/death.png",    1, 28, 28),
-    decoy		        = Sprite.new("sGrapplerDecoy",		"Sprites/decoy.png",    1, 18, 36)
+    idle			    = Sprite.new("sGrapplerIdle",       "Sprites/idle.png",     1, 0, 0),
+    walk		        = Sprite.new("sGrapplerWalk",		"Sprites/walk.png",     1, 0, 0),
+    jump		        = Sprite.new("sGrapplerJump",		"Sprites/jump.png",     1, 0, 0),
+    jump_peak	        = Sprite.new("sGrapplerJumpPeak",	"Sprites/jumppeak.png", 1, 0, 0),
+    lfall		        = Sprite.new("sGrapplerFall",		"Sprites/fall.png",     1, 0, 0),
+    climb		        = Sprite.new("sGrapplerClimb",		"Sprites/climb.png",    1, 0, 0),
+    climb_hurt	        = Sprite.new("sGrapplerClimbHurt",	"Sprites/climb.png",    1, 0, 0),
+    death		        = Sprite.new("sGrapplerDeath",		"Sprites/death.png",    1, 0, 0),
+    decoy		        = Sprite.new("sGrapplerDecoy",		"Sprites/decoy.png",    1, 0, 0)
 }
 
 -- Create a table with the ability animations (referred to as "shoot")
 local sGrapplerShoot = {
-    shoot1_a            = Sprite.new("sGrapplerShoot1_a",   "Sprites/shoot1_a",     1, 23, 38),
-    shoot1_b            = Sprite.new("sGrapplerShoot1_b",   "Sprites/shoot1_b",     1, 23, 38),
-    shoot1_c            = Sprite.new("sGrapplerShoot1_c",   "Sprites/shoot1_c",     1, 23, 38),
-    shoot2              = Sprite.new("sGrapplerShoot2",     "Sprites/shoot2",       1, 23, 38),
-    shoot3              = Sprite.new("sGrapplerShoot3",     "Sprites/shoot3",       1, 23, 38),
-    shoot4              = Sprite.new("sGrapplerShoot4",     "Sprites/shoot4",       1, 23, 38)
+    shoot1_a            = Sprite.new("sGrapplerShoot1_a",   "Sprites/shoot1_a",     1, 0, 0),
+    shoot1_b            = Sprite.new("sGrapplerShoot1_b",   "Sprites/shoot1_b",     1, 0, 0),
+    shoot1_c            = Sprite.new("sGrapplerShoot1_c",   "Sprites/shoot1_c",     1, 0, 0),
+    shoot2              = Sprite.new("sGrapplerShoot2",     "Sprites/shoot2",       1, 0, 0),
+    shoot3              = Sprite.new("sGrapplerShoot3",     "Sprites/shoot3",       1, 0, 0),
+    shoot4              = Sprite.new("sGrapplerShoot4",     "Sprites/shoot4",       1, 0, 0)
 }
 
 local sGrapplerSkills = Sprite.new("sGrapplerSkills", "Sprites/skills", 5)
